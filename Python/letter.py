3488f32d54d2a95325f5536da79b66be807276e5
Brett Lindenberg <balindenberg@gmail.com>
2015-10-22 16:25:06 -0400

    UF OSM query working

--- Python/letter.py
+++ Python/letter.py
@@ -4,7 +4,7 @@ api = overpy.Overpass()
 print("Querying UF Campus with coordinates (-82.355444, 29.635203, 82.339565, 29.652099) ...")
 # result = api.query("node(-82.355444, 29.635203, 82.339565, 29.652099)[highway=motorway];out;")
 
-result = api.query("""way(50.745,7.17,50.75,7.18)["highway"];
+result = api.query("""way(29.627404, -82.372599, 29.652378, -82.339341)["highway"];
     (._;>;);
     out body;
     """)
@@ -14,3 +14,4 @@ for node in result.ways:
 	print "Node id: ", node.id
 	# print "Node lat/lon: ", node.lat, node.lon
 	print "Node tags: ", node.tags
+	print Node.name

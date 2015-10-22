6e6fdbf1a02883d55e68aa003d2e2be16837c1a4
Brett Lindenberg <balindenberg@gmail.com>
2015-10-22 17:08:19 -0400

    Alphabetical street printing

--- Python/letter.py
+++ Python/letter.py
@@ -8,7 +8,7 @@ def searchForName(values, searchFor):
     return None
 
 api = overpy.Overpass()
-print("Querying UF Campus with coordinates (-82.355444, 29.635203, 82.339565, 29.652099) ...")
+print("Querying UF Campus with coordinates (29.627404, -82.372599, 29.652378, -82.339341) ...")
 # result = api.query("node(-82.355444, 29.635203, 82.339565, 29.652099)[highway=motorway];out;")
 
 result = api.query("""way(29.627404, -82.372599, 29.652378, -82.339341)["highway"];
@@ -17,11 +17,17 @@ result = api.query("""way(29.627404, -82.372599, 29.652378, -82.339341)["highway
     """)
 print "Number of nodes: ", len(result.nodes)
 print "Number of ways: ", len(result.ways)
+s = []
 for node in result.ways:
 	# print "Node id: ", node.id
 	# print "Node lat/lon: ", node.lat, node.lon
 	# print "Node tags: ", node.tags
 	if "name" in node.tags:
-		print node.tags.get("name")
+		name = node.tags.get("name")
+		if name not in s:
+			s.append(node.tags.get("name"));
 
+s = sorted(s)
+for street in s:
+	print street
 

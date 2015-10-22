fbd1c757ab860ff87199efa672e04442df6df0d1
Brett Lindenberg <balindenberg@gmail.com>
2015-10-22 16:22:51 -0400

    miguel updates

--- Python/letter.py
+++ Python/letter.py
@@ -4,10 +4,13 @@ api = overpy.Overpass()
 print("Querying UF Campus with coordinates (-82.355444, 29.635203, 82.339565, 29.652099) ...")
 # result = api.query("node(-82.355444, 29.635203, 82.339565, 29.652099)[highway=motorway];out;")
 
-result = api.query("node(11.54,48.14,11.543,48.145);out;")
+result = api.query("""way(50.745,7.17,50.75,7.18)["highway"];
+    (._;>;);
+    out body;
+    """)
 print "Number of nodes: ", len(result.nodes)
 print "Number of ways: ", len(result.ways)
-for node in result.nodes:
+for node in result.ways:
 	print "Node id: ", node.id
-	print "Node lat/lon: ", node.lat, node.lon
+	# print "Node lat/lon: ", node.lat, node.lon
 	print "Node tags: ", node.tags
